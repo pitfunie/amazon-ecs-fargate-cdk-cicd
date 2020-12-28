1177e6d8d3b2844cd51cd4397fb61f33a4ddcfdf
pitfunie <pitfunie@gmail.com>
2020-12-28 11:17:58 -0500

    added cicd-fargate-commands.sh file

---
+++ cicd-fargate-commands.sh
@@ -0,0 +1,12 @@
+# This project helps you build a complete Amazon ECS cluster and CI/CD pipeline with CodeBuild in AWS CDK.
+
+# Step1. Cloud9 and commands to run: Procedures 
+
+https://github.com/aws-samples/amazon-ecs-fargate-cdk-cicd
+
+# However using MacOS Catalina
+
+# Configure the Repository by forking the repository to into my account pitfunie@gmail.com
+
+https://github.com/aws-samples/amazon-ecs-fargate-cdk-cicd
+
